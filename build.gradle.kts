7f4ba24fe9bcec3ca0cd8ed35f59c65a5fd78c4b
Edward Harman <jaq+github@ethelred.org>
2025-03-02 10:51:01 -0500

    publishing config

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+}
+
+group = "org.ethelred.kiwiproc"
+version = "0.1"
+
+nexusPublishing {
+	repositories {
+		create("sonatype") {
+			//only for users registered in Sonatype after 24 Feb 2021
+			nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+			snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+		}
+	}
+}
