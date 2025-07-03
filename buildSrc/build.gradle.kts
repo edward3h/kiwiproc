14f48c52f7da6574d19f928067f55d4bd4ae911f
Edward Harman <jaq+github@ethelred.org>
2025-07-03 11:03:53 -0400

    Remove leftover dependencies in buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,7 +14,5 @@ java {
 }
 
 dependencies {
-    implementation(libs.embeddedpostgres)
     implementation("com.diffplug.spotless:spotless-plugin-gradle:7.0.4")
-    runtimeOnly(libs.liquibase.core)
 }
