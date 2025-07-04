2ba721f4e317eb56456984fa6d69b4d50b507a03
Edward Harman <jaq+github@ethelred.org>
2025-07-04 15:35:55 -0400

    Prepare to publish 0.3.2

--- publish.bash
+++ publish.bash
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ $(git status --porcelain=1 | wc -l) -ne 0 ]; then
+  echo "There are local modifications. Resolve before publishing."
+  exit 1
+fi
+
 if grep SNAPSHOT version.gradle.kts; then
   echo "Set a non-snapshot version, then run this script."
   exit 2
@@ -9,9 +14,14 @@ fi
 
 # publish plugin library to maven
 pushd gradle-plugin/
+gw clean
+gw build
 gw publishAllPublicationsToProjectLocalRepository zipMavenCentralPortalPublication releaseMavenCentralPortalPublication
 popd
 # publish main libraries to maven
+gw clean
+gw build
+
 gw publishAllPublicationsToProjectLocalRepository zipMavenCentralPortalPublication releaseMavenCentralPortalPublication
 # publish gradle plugin
 pushd gradle-plugin/
