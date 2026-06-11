9667653d44b1bbb3183791ae9eb8107505e494e5
Edward Harman <jaq+github@ethelred.org>
2026-06-11 16:13:48 -0400

    build: mark org.ethelred.kiwiproc.maven package @NullMarked
    Matches the project's jspecify nullability convention, consistent
    with the processor module's packages. DataSourceParameter already
    uses @Nullable, which is more meaningful with a non-null default.

--- gradle-plugin/maven-plugin/src/main/java/org/ethelred/kiwiproc/maven/package-info.java
+++ gradle-plugin/maven-plugin/src/main/java/org/ethelred/kiwiproc/maven/package-info.java
@@ -1,3 +1,6 @@
 /* (C) Edward Harman 2026 */
 /** Maven plugin equivalent of the Gradle kiwiproc plugin. */
+@NullMarked
 package org.ethelred.kiwiproc.maven;
+
+import org.jspecify.annotations.NullMarked;
